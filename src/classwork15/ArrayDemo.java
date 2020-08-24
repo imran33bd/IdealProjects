7b308150e2ac668a3c5370a4841ada995902f14f
imran33bd <imran_nsu2003@hotmail.com>
2020-08-24 01:35:33 -0400

    updated version

--- src/classwork15/ArrayDemo.java
+++ src/classwork15/ArrayDemo.java
@@ -1,28 +1,19 @@
 package classwork15;
 
 public class ArrayDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // How to declare and initialize an array?
 
         int[] studentID = new int[5];
         // How to insert value in an array in a specific index
 
         // Probable area of problem
-        try {
-            studentID[0] = 101;
-            studentID[1] = 102;
-            studentID[2] = 103;
-            studentID[3] = 104;
-            studentID[4] = 105;
-            //studentID[5] = 106;
-        }
-
-        catch (Exception problem){
-            // Show me the problem
-            problem.printStackTrace();
-            // Provide a nice meaningful message to the user
-            System.out.println("You are trying to insert more than 5 element");
-        }
+        studentID[0] = 101;
+        studentID[1] = 102;
+        studentID[2] = 103;
+        studentID[3] = 104;
+        studentID[4] = 105;
+        //studentID[5] = 106;
 
         // How to get value from a single index?
         /*System.out.println("Get the element in the index 0: " + studentID[0]);
